94a33b08a23e3af8283a960a457abc485548e683
Krystal T Le <krystaltle94@gmail.com>
2017-02-02 17:21:12 +0000

    comment changes

--- first.rb
+++ first.rb
@@ -1,9 +1,25 @@
-print "Enter your name: "
-name=gets.chop
-puts "Hello " << name
+# print "Enter your name: "
+# name=gets.chop
+# puts "Hello " << name
 
-print "Enter number of cups: "
-cups=gets.chop
-cups=cups.to_i
-ounces=cups*8
-puts "That is #{ounces} ounces"
+# print "Enter number of cups: "
+# cups=gets.chop
+# cups=cups.to_i
+# ounces=cups*8
+# puts "That is #{ounces} ounces"
+
+#Playing with strings
+print "Enter your full name: "
+
+name=gets.chomp
+puts name.upcase
+puts name.downcase
+puts name.capitalize 
+puts name
+
+
+names.name.split(" ")
+first=names[0]
+last=names[1]
+puts first
+puts last
--- guessNumber.rb
+++ guessNumber.rb
@@ -1,11 +1,15 @@
 #This program generates a random number. User enters their guess.
-#The program will keep asking for input until the user input and the random number matches
-#User has total of 3 tries.
+#The program will keep asking for input user guesses the right number or the 
+#max tries(3) is reached; whatever comes first
 #Krystal Le
 #Spring 2017. Software Engineering.
+
+#Generate random number between 0 and 10
 random_number = rand(11)
-#Num of tres
+
+#Num of tries
 num_try=2
+
 print "Enter a number between 0 and 10: "
 while user_input=gets.to_i and num_try>0
     if user_input == random_number
@@ -20,5 +24,5 @@ while user_input=gets.to_i and num_try>0
     #Decrement try every time user inputs
     num_try-=1
 end
-#End program output when user reaches max tries
+#Output the result when max tries is reached
 puts "Maximum tries reached. The number is: #{num_try}"
