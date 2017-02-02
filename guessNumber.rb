d6249ddbbec7a4f87da7a9e5535b9e0c3db7eca6
Krystal T Le <krystaltle94@gmail.com>
2017-02-02 17:12:02 +0000

    tries added

--- guessNumber.rb
+++ guessNumber.rb
@@ -1,6 +1,12 @@
+#This program generates a random number. User enters their guess.
+#The program will keep asking for input until the user input and the random number matches
+#User has total of 3 tries.
+#Krystal Le
+#Spring 2017. Software Engineering.
 random_number = rand(11)
+num_try=2
 print "Enter a number between 0 and 10: "
-while user_input=gets.to_i
+while user_input=gets.to_i and num_try>0
     if user_input == random_number
         puts "You got it"
         break
@@ -8,5 +14,8 @@ while user_input=gets.to_i
         print "Enter a bigger number: "
     else
         print "Enter a smaller number: "
+
     end
+    num_try-=1
 end
+puts "Maximum tries reached. The number is: #{num_try}"
